077b191d4b6e1a2d2f1c2f10836c462bc5325a80
Khalid <alhajr20@gmail.com>
2021-04-04 22:06:50 +0300

    Created header and footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { Container } from '../components/Container';
 
-export default function Home() {
-    return <div>Hello world!</div>
-}
+const IndexPage = () => {
+    return (
+        <Container></Container>
+    );
+};
+
+export default IndexPage;
